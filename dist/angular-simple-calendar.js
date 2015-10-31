0fcd0c1ce45fe37e78b776d1d79be964c6bc56c6
Laurens <l.maarschalkerweerd@gmail.com>
2015-10-31 15:07:51 +0100

    Added support for Monday as first date
    Add firstDateIsMonday = true to calendar options to render the calendar with Monday as the first date of the week.

--- dist/angular-simple-calendar.js
+++ dist/angular-simple-calendar.js
@@ -1,208 +1,115 @@
-angular.module('500tech.simple-calendar', []).directive('simpleCalendar', function () {
-  return {
-    restrict: 'E',
-    scope: {
-      options: '=?',
-      events: '=?'
-    },
-    template: '<div class="calendar">' +
-      '<div class="current-month">' +
-      '<div class="move-month prev-month" ng-click="prevMonth()">' +
-      '<span ng-show="allowedPrevMonth()">&#x2039;</span>' +
-      '</div>' +
-      '<span>{{ selectedMonth }}</span>' +
-      '&nbsp;' +
-      '<span>{{ selectedYear }}</span>' +
-      '<div class="move-month next-month" ng-click="nextMonth()">' +
-      '<span ng-show="allowedNextMonth()">&#x203a;</span>' +
-      '</div>' +
-      '</div>' +
-      '<div>' +
-      '<div ng-repeat="day in weekDays(options.dayNamesLength) track by $index" class="weekday">{{ day }}</div>' +
-      '</div>' +
-      '<div>' +
-      '<div ng-repeat="week in weeks track by $index" class="week">' +
-      '<div class="day"' +
-      'ng-class="{default: isDefaultDate(date), event: date.event, disabled: date.disabled || !date}"' +
-      'ng-repeat="date in week  track by $index"' +
-      'ng-click="onClick(date)">' +
-      '<div class="day-number">{{ date.day || "&nbsp;" }}</div>' +
-      '<div class="event-title">{{ date.event.title || "&nbsp;" }}</div>' +
-      ' </div>' +
-      '</div>' +
-      '</div>' +
-      '</div>',
-    controller: ['$scope', function ($scope) {
-      var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      var WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      var calculateSelectedDate, calculateWeeks, allowedDate, bindEvent;
+/**=========================================================
+ * Module: new-requests.controller.js
+ * http://onehungrymind.com/build-angularjs-wizard-redux/
+ =========================================================*/
+(function() {
+    'use strict';
 
-      $scope.options = $scope.options || {};
-      $scope.options.dayNamesLength = $scope.options.dayNamesLength || 1;
+    angular
+        .module('app.requests')
+        .controller('NewRequestsController', NewRequestsController);
 
-      $scope.onClick = function (date) {
-        if (!date || date.disabled) { return; }
-        if (date.event) {
-          $scope.options.eventClick(date);
-        } else {
-          $scope.options.dateClick(date);
-        }
-      };
+    NewRequestsController.$inject = ['$timeout', 'Autocomplete'];
+    
+    function NewRequestsController($timeout, Autocomplete) {
+ 		var modal = this;
+ 		
+        activate();	
 
-      if ($scope.options.minDate) {
-        $scope.options.minDate = new Date($scope.options.minDate);
-      }
+        ////////////////        
 
-      if ($scope.options.maxDate) {
-        $scope.options.maxDate = new Date($scope.options.maxDate);
-      }
+        function activate() {
+	        modal.autocompleteAirports	= function(q) { return Autocomplete.Airports(q); };
+	        
+	        // Modal definition functions
+			modal.steps = ['flight', 'date', 'confirm', 'pay', 'success'];
+			modal.step = 1;  ////////// TEMP
+			modal.data = {};
+			
+			// Modal functions
+			modal.isCurrentStep = function(step) {
+			    return modal.step === step;
+			};
+			
+			modal.setCurrentStep = function(step) {
+			    modal.step = step;
+			};
+			
+			modal.getCurrentStep = function() {
+			    return modal.steps[modal.step];
+			};			
+			
+			modal.isFirstStep = function() {
+			    return modal.step === 0;
+			};
+			
+			modal.isLastStep = function() {
+			    return modal.step === (modal.steps.length - 1);
+			};
+			
+			modal.getNextLabel = function() {
+				if (modal.isCurrentStep(0)) return 'Pick date';
+				if (modal.isCurrentStep(1)) return 'Next';
+				if (modal.isCurrentStep(2)) return 'Confirm';
+				if (modal.isCurrentStep(3)) return 'Next';
+				if (modal.isCurrentStep(4)) return 'Close';
+			};
+			
+			modal.handlePrevious = function() {
+			    modal.direction = 'previous';
+				$timeout(function() {
+					modal.step -= (modal.isFirstStep()) ? 0 : 1;
+				}, 1);
+			    
+			};
+			
+			modal.handleNext = function() {
+			    if (modal.isLastStep()) {
+			        $modalInstance.close(modal.wizard);
+			    } else {
+			        modal.direction = 'next';
+					$timeout(function() {
+						 modal.step += 1;
+					}, 1);			        
+			    }
+			};
+			
+			modal.selectAirport = function(airport) {
+				Autocomplete.AirportsHistory.Push(airport.icao);
+				modal.focus = 'to';
+			};
+			
+			
+			// Validation
+			
+			// step 0 - flight
+			modal.isValid = function () {
+				if (modal.isCurrentStep(0)) {
+					if (modal.data.flightnr || (modal.data.from && modal.data.to)) return true;
+				}
+			}
+			
+			
+			// Calendar
+			var today = new Date();
+			var yesterday = new Date();
+			yesterday.setDate(-1);
+			
+			modal.calendarOptions = {
+				firstDayIsMonday: true,
+				defaultDate: today,
+				minDate: yesterday,
+				dayNamesLength: 1, // How to display weekdays (1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names; default is 1)
+				dateClick: function(date) { console.log(date) }
+			};
+			
+			modal.dateClick = function(date) {
+				console.log(date);
+			};
+			
 
-      bindEvent = function (date) {
-        if (!date || !$scope.events) { return; }
-        $scope.events.forEach(function(event) {
-          event.date = new Date(event.date);
-          if (date.year === event.date.getFullYear() && date.month === event.date.getMonth() && date.day === event.date.getDate()) {
-            date.event = event;
-          }
-        });
-      };
-
-      allowedDate = function (date) {
-        if (!$scope.options.minDate && !$scope.options.maxDate) {
-          return true;
-        }
-        var currDate = new Date([date.year, date.month + 1, date.day]);
-        if ($scope.options.minDate && (currDate < $scope.options.minDate)) { return false; }
-        if ($scope.options.maxDate && (currDate > $scope.options.maxDate)) { return false; }
-        return true;
-      };
-
-      $scope.allowedPrevMonth = function () {
-        var prevYear = null;
-        var prevMonth = null;
-        if (!$scope.options.minDate) { return true; }
-        var currMonth = MONTHS.indexOf($scope.selectedMonth);
-        if (currMonth === 0) {
-          prevYear = ($scope.selectedYear - 1);
-        } else {
-          prevYear = $scope.selectedYear;
-        }
-        if (currMonth === 0) {
-          prevMonth = 11;
-        } else {
-          prevMonth = (currMonth - 1);
-        }
-        if (prevYear < $scope.options.minDate.getFullYear()) { return false; }
-        if (prevYear === $scope.options.minDate.getFullYear()) {
-          if (prevMonth < $scope.options.minDate.getMonth()) { return false; }
-        }
-        return true;
-      };
-
-      $scope.allowedNextMonth = function () {
-        var nextYear = null;
-        var nextMonth = null;
-        if (!$scope.options.maxDate) { return true; }
-        var currMonth = MONTHS.indexOf($scope.selectedMonth);
-        if (currMonth === 11) {
-          nextYear = ($scope.selectedYear + 1);
-        } else {
-          nextYear = $scope.selectedYear;
-        }
-        if (currMonth === 11) {
-          nextMonth = 0;
-        } else {
-          nextMonth = (currMonth + 1);
-        }
-        if (nextYear > $scope.options.maxDate.getFullYear()) { return false; }
-        if (nextYear === $scope.options.maxDate.getFullYear()) {
-          if (nextMonth > $scope.options.maxDate.getMonth()) { return false; }
-        }
-        return true;
-      };
-
-      calculateWeeks = function () {
-        $scope.weeks = [];
-        var week = null;
-        var daysInCurrentMonth = new Date($scope.selectedYear, MONTHS.indexOf($scope.selectedMonth) + 1, 0).getDate();
-        for (var day = 1; day < daysInCurrentMonth + 1; day += 1) {
-          var dayNumber = new Date($scope.selectedYear, MONTHS.indexOf($scope.selectedMonth), day).getDay();
-          week = week || [null, null, null, null, null, null, null];
-          week[dayNumber] = {
-            year: $scope.selectedYear,
-            month: MONTHS.indexOf($scope.selectedMonth),
-            day: day
-          };
-
-          if (allowedDate(week[dayNumber])) {
-            if ($scope.events) { bindEvent(week[dayNumber]); }
-          } else {
-            week[dayNumber].disabled = true;
-          }
-
-          if (dayNumber === 6 || day === daysInCurrentMonth) {
-            $scope.weeks.push(week);
-            week = undefined;
-          }
-        }
-      };
-
-      calculateSelectedDate = function () {
-        if ($scope.options.defaultDate) {
-          $scope.options._defaultDate = new Date($scope.options.defaultDate);
-        } else {
-          $scope.options._defaultDate = new Date();
-        }
-
-        $scope.selectedYear  = $scope.options._defaultDate.getFullYear();
-        $scope.selectedMonth = MONTHS[$scope.options._defaultDate.getMonth()];
-        $scope.selectedDay   = $scope.options._defaultDate.getDate();
-        calculateWeeks();
-      };
-
-      $scope.weekDays = function (size) {
-        return WEEKDAYS.map(function(name) { return name.slice(0, size) });
-      };
-
-      $scope.isDefaultDate = function (date) {
-        if (!date) { return; }
-        return date.year === $scope.options._defaultDate.getFullYear() &&
-          date.month === $scope.options._defaultDate.getMonth() &&
-          date.day === $scope.options._defaultDate.getDate()
-      };
-
-      $scope.prevMonth = function () {
-        if (!$scope.allowedPrevMonth()) { return; }
-        var currIndex = MONTHS.indexOf($scope.selectedMonth);
-        if (currIndex === 0) {
-          $scope.selectedYear -= 1;
-          $scope.selectedMonth = MONTHS[11];
-        } else {
-          $scope.selectedMonth = MONTHS[currIndex - 1];
-        }
-        calculateWeeks();
-      };
-
-      $scope.nextMonth = function () {
-        if (!$scope.allowedNextMonth()) { return; }
-        var currIndex = MONTHS.indexOf($scope.selectedMonth);
-        if (currIndex === 11) {
-          $scope.selectedYear += 1;
-          $scope.selectedMonth = MONTHS[0];
-        } else {
-          $scope.selectedMonth = MONTHS[currIndex + 1];
-        }
-        calculateWeeks();
-      };
-
-      $scope.$watch('options.defaultDate', function() {
-        calculateSelectedDate();
-      });
-
-      $scope.$watch('events', function() {
-        calculateWeeks();
-      });
-
-    }]
-  }
-});
+						
+        };
+    }  
+    
+})();
